d248fde2f8fe67b11fc4c368e7fed0b7ca136f31
hdgomez8 <hdgomez8@hotmail.com>
2023-09-03 23:36:09 -0500

    Antes Del 1 Sprint Actualizado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,29 @@
-
 import React from "react";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
-import ProductDetail from './views/ProductDetail';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ProductDetail from "./views/Detail/ProductDetail";
 import Landing from "./components/Landing/Landing";
 import NavBar from "./components/NavBar/NavBar";
-import Home from "./views/Home/Home";
 import Footer from "./components/Footer/Footer";
-import Product from "./components/Product/Product";
 import Ofertas from "./views/Ofertas/Ofertas";
-import axios from "axios";
-axios.defaults.baseURL="https://dlca-deploy-production.up.railway.app/";
+import Form from "./views/Form/FormProduct";
+import Productos from "./views/Productos/Productos";
 
 const App = () => {
-
   return (
-
-    <Routes>
-      <Route path="/">
-        <Landing />
-          <Route path="/" component={NavBar}>
-          <Route path="/home" component={Home} />
-          <Route path="/product" component={<Product />} />
-          <Route path='/products/ofertas' component={<Ofertas/>}/>
-          <Route path="/product/:productId" component={ProductDetail} />
-        </Route>
-      </Route>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route path="/productos" component={Productos} />
+        <Route path="/product/:id" component={ProductDetail} />
+        <Route path="/form" component={Form} />
+        <Route exact path="/" component={Landing} />
+      </Switch>
       <Footer />
-    </Routes>
+    </Router>
   );
 };
 
 export default App;
+
+
+
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,23 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from './redux/store';
-import { Provider } from 'react-redux';
+import { store } from '../src/redux/store/index';
+import { Provider, useSelector } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
+import { createRoot } from 'react-dom/client';
 
-// Cambiar para el deploy/
+// Cambiar para el deploy
+
 // axios.defaults.baseURL = "https://LINK-PARA-DEPLOY"; 
 axios.defaults.baseURL = "http://localhost:3001";
-//const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.render(
+const root = document.getElementById('root');
+const rootElement = createRoot(root);
+
+const DarkModeApp = () => {
+  const darkMode = useSelector((state) => state.darkMode); // Obtiene el estado de darkMode
+
+  return (
+    <div className={darkMode ? "dark-mode" : ""}>
+      <App />
+    </div>
+  );
+};
+
+
+rootElement.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
 
 reportWebVitals();
